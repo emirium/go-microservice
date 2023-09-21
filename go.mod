df24af6097e8b2e519eb1e0d83567f204a5d87d4
Emir Smajic <emsmjc@smajic.local>
2023-09-21 05:09:32 +0200

    chi router and middleware added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/emirium/go-microservice
+
+go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+	
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server", err)
+	}
+}
