cca9c104721492909987b11ce1be7cb593e51462
Emir Smajic <emsmjc@smajic.local>
2023-09-22 04:55:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/emirium/go-microservice/application"
 )
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("Failed to start the app:", err)
 	}
+
+	cancel()
 }
